06637feae921efb202168b0f18362eed373070bc
= <antoniochavezmarco@gmail.com>
2023-11-30 21:43:09 -0600

    Little changes

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -34,11 +34,11 @@ class EventController extends Controller
                 $message = "Evento creado con éxito";
             } else {
                 $success = false;
-                $message = "No tienes permisos para realizar esta acción";
+                $message = "No tienes permisos para realizar esta acción 1";
             }
         } else {
             $success = false;
-            $message = "No tienes permisos para realizar esta acción";
+            $message = "No tienes permisos para realizar esta acción 2";
         }
         return response()->json([
             'success'=>$success,
